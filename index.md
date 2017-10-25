29edcf72991e9b5dbfa790bb97fd10960660e239
Pavel Vavilin <shtartora@gmail.com>
2017-10-25 14:39:58 +0500

    Now index is with css and with numerated list

--- index.md
+++ index.md
@@ -1,8 +1,6 @@
 <link rel="stylesheet" type="text/css" href="solarized-dark.css" />
 
-Index Page
-==========
-4. [C++ named cast](https://pimiento.github.io/cast.html "C++ named cast")
-3. [переменное число аргументов функции в C](https://pimiento.github.io/c_varargs.html "переменное число аргументов функции в C")
-2. [структуры в C](https://pimiento.github.io/c_structures.html "структуры в C")
-1. [Декораторы в Python](https://pimiento.github.io/decorators.html "Декораторы в Python")
+1. [C++ named cast](https://pimiento.github.io/cast.html "C++ named cast")
+2. [переменное число аргументов функции в C](https://pimiento.github.io/c_varargs.html "переменное число аргументов функции в C")
+3. [структуры в C](https://pimiento.github.io/c_structures.html "структуры в C")
+4. [Декораторы в Python](https://pimiento.github.io/decorators.html "Декораторы в Python")
--- index_generator.py
+++ index_generator.py
@@ -8,16 +8,20 @@ def get_pages():
     pages = sorted(glob.iglob("*.html"),
                    key=lambda x: os.stat(x).st_mtime,
                    reverse=True)
+    counter = 1
 
     for page in pages:
         tree = etree.parse(page, etree.HTMLParser())
         title = tree.xpath("/html/head/title")[0].text
         url = "https://pimiento.github.io/{}".format(page)
-        yield("[{title}]({url} \"{title}\")".format(title=title, url=url))
+        yield("{N}. [{title}]({url} \"{title}\")".format(
+            N=counter, title=title, url=url
+        ))
+        counter += 1
 
 
 if __name__ == "__main__":
     with open("index.md", "w") as md:
-        print("# Index Page #", file=md)
-        print("", file=md)
+        css = """<link rel="stylesheet" type="text/css" href="solarized-dark.css" />"""
+        print(css, end="\n\n", file=md)
         print(*get_pages(), file=md, sep='\n')
