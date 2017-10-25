ea5aed0901976e68433ce89829b0c5529e1a7e29
Pavel Vavilin <shtartora@gmail.com>
2017-10-25 14:17:47 +0500

    script to generate index file

---
+++ index_generator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+import glob
+from lxml import etree
+
+
+def get_pages():
+    pages = sorted(glob.iglob("*.html"),
+                   key=lambda x: os.stat(x).st_mtime,
+                   reverse=True)
+
+    for page in pages:
+        tree = etree.parse("cast.html", etree.HTMLParser())
+        title = tree.xpath("/html/head/title")[0].text
+        url = "https://pimiento.github.io/{}".format(page)
+        yield("[{title}]({url} \"{title}\")".format(title=title, url=url))
+
+
+if __name__ == "__main__":
+    with open("index.md", "w") as md:
+        print("# Index Page #", file=md)
+        print("", file=md)
+        print(*get_pages(), file=md, sep='\n')
