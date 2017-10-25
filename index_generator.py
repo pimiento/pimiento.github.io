16b8e19a1ea68859963dd283e4edaf3376dacbdf
Pavel Vavilin <shtartora@gmail.com>
2017-10-25 14:43:35 +0500

    numbers doesn't work anyway

--- index_generator.py
+++ index_generator.py
@@ -8,16 +8,12 @@ def get_pages():
     pages = sorted(glob.iglob("*.html"),
                    key=lambda x: os.stat(x).st_mtime,
                    reverse=True)
-    counter = 1
 
     for page in pages:
         tree = etree.parse(page, etree.HTMLParser())
         title = tree.xpath("/html/head/title")[0].text
         url = "https://pimiento.github.io/{}".format(page)
-        yield("{N}. [{title}]({url} \"{title}\")".format(
-            N=counter, title=title, url=url
-        ))
-        counter += 1
+        yield("* [{title}]({url} \"{title}\")".format(title=title, url=url))
 
 
 if __name__ == "__main__":
